ad8e6c0f39f39e0ce490bf4e9df256a7741e8c44
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-05-01 21:35:55 -0300

    Update

--- app/src/main/java/br/com/renandeldotti/inventoryapp/database/InventoryDatabase.java
+++ app/src/main/java/br/com/renandeldotti/inventoryapp/database/InventoryDatabase.java
@@ -3,6 +3,6 @@ package br.com.renandeldotti.inventoryapp.database;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Products.class,OutProducts.class}, version = 1)
+@Database(entities = {Products.class}, version = 1)
 abstract class InventoryDatabase extends RoomDatabase {
 }
--- app/src/main/java/br/com/renandeldotti/inventoryapp/database/Products.java
+++ app/src/main/java/br/com/renandeldotti/inventoryapp/database/Products.java
@@ -1,5 +1,6 @@
 package br.com.renandeldotti.inventoryapp.database;
 
+import androidx.annotation.Nullable;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.ForeignKey;
@@ -17,5 +18,46 @@ class Products {
 
     private int product_quantity;
 
+    private int quantity_sold;
+
     private float price;
+
+    public Products(String product_name, String product_description, int product_quantity, float price) {
+        this.product_name = product_name;
+        this.product_description = product_description;
+        this.product_quantity = product_quantity;
+        this.price = price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setQuantity_sold(int quantity_sold) {
+        this.quantity_sold = quantity_sold;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+
+    public String getProduct_description() {
+        return product_description;
+    }
+
+    public int getProduct_quantity() {
+        return product_quantity;
+    }
+
+    public int getQuantity_sold() {
+        return quantity_sold;
+    }
+
+    public float getPrice() {
+        return price;
+    }
 }
