fc6fe34509d69f9fbbfe1f4824696332e0ec0ef5
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-05-02 16:42:10 -0300

    Update

--- app/src/main/java/br/com/renandeldotti/inventoryapp/ui/sales/SalesFragment.java
+++ app/src/main/java/br/com/renandeldotti/inventoryapp/ui/sales/SalesFragment.java
@@ -4,12 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 
 import br.com.renandeldotti.inventoryapp.R;
 
@@ -18,7 +15,7 @@ public class SalesFragment extends Fragment {
     private SalesViewModel notificationsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        View root = inflater.inflate(R.layout.fragment_sales, container, false);
         return root;
     }
 }
