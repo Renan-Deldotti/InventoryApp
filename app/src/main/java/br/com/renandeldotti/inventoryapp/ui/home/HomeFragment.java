4c1f33fe8ed205adcf5aaa090c34408d1d1ce88d
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-05-06 00:07:31 -0300

    Update

--- app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeFragment.java
+++ app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -14,22 +15,23 @@ import br.com.renandeldotti.inventoryapp.R;
 
 public class HomeFragment extends Fragment {
 
-    private Button buttonShow;
-    private boolean isButtonShowVisible = false;
+    private TextView buttonShow;
+    private int isButtonShowVisible;
 
     public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         buttonShow = root.findViewById(R.id.home_buttonShow);
+        isButtonShowVisible = buttonShow.getVisibility();
 
         root.findViewById(R.id.ritmo_vendas).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (isButtonShowVisible){
+                if (isButtonShowVisible == View.VISIBLE){
                     buttonShow.setVisibility(View.GONE);
-                    isButtonShowVisible = false;
+                    isButtonShowVisible = View.GONE;
                 }else {
                     buttonShow.setVisibility(View.VISIBLE);
-                    isButtonShowVisible = true;
+                    isButtonShowVisible = View.VISIBLE;
                 }
             }
         });
