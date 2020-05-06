8fa46bcae751c7252e26bb202f43ce67dc90e2d9
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-05-06 01:15:20 -0300

    Layout updates

--- app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeFragment.java
+++ app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeFragment.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -15,26 +13,29 @@ import br.com.renandeldotti.inventoryapp.R;
 
 public class HomeFragment extends Fragment {
 
-    private TextView buttonShow;
-    private int isButtonShowVisible;
+    private View tvRitmoVendas;
+    private int isTvRitmoVendasVisible;
 
     public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        buttonShow = root.findViewById(R.id.home_buttonShow);
-        isButtonShowVisible = buttonShow.getVisibility();
+
+        tvRitmoVendas = root.findViewById(R.id.home_buttonShow);
+        isTvRitmoVendasVisible = tvRitmoVendas.getVisibility();
 
         root.findViewById(R.id.ritmo_vendas).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (isButtonShowVisible == View.VISIBLE){
-                    buttonShow.setVisibility(View.GONE);
-                    isButtonShowVisible = View.GONE;
+                if (isTvRitmoVendasVisible == View.VISIBLE){
+                    tvRitmoVendas.setVisibility(View.GONE);
+                    isTvRitmoVendasVisible = View.GONE;
                 }else {
-                    buttonShow.setVisibility(View.VISIBLE);
-                    isButtonShowVisible = View.VISIBLE;
+                    tvRitmoVendas.setVisibility(View.VISIBLE);
+                    isTvRitmoVendasVisible = View.VISIBLE;
                 }
             }
         });
+
+
         root.findViewById(R.id.aumento_vendas).setOnClickListener(clickListener);
         root.findViewById(R.id.queda_vendas).setOnClickListener(clickListener);
         root.findViewById(R.id.total_vendas).setOnClickListener(clickListener);
@@ -43,7 +44,7 @@ public class HomeFragment extends Fragment {
         return root;
     }
 
-    View.OnClickListener clickListener = new View.OnClickListener() {
+    private View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Toast.makeText(getActivity(), "Todo: Criar tela...", Toast.LENGTH_SHORT).show();
