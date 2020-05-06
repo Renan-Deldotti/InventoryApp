9b86efa222958f13f458ff68e29585e47f7b85a4
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-05-05 23:30:23 -0300

    Update
    Deleted total sales activity and it's layout.
    
    Some layout fixes.

--- app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeFragment.java
+++ app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeFragment.java
@@ -1,33 +1,41 @@
 package br.com.renandeldotti.inventoryapp.ui.home;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import br.com.renandeldotti.inventoryapp.R;
-import br.com.renandeldotti.inventoryapp.TotalSalesActivity;
 
 public class HomeFragment extends Fragment {
 
+    private Button buttonShow;
+    private boolean isButtonShowVisible = false;
+
     public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        buttonShow = root.findViewById(R.id.home_buttonShow);
 
-        root.findViewById(R.id.ritmo_vendas).setOnClickListener(clickListener);
-        root.findViewById(R.id.aumento_vendas).setOnClickListener(clickListener);
-        root.findViewById(R.id.queda_vendas).setOnClickListener(clickListener);
-        root.findViewById(R.id.total_vendas).setOnClickListener(new View.OnClickListener() {
+        root.findViewById(R.id.ritmo_vendas).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getContext(), TotalSalesActivity.class);
-                startActivity(intent);
+                if (isButtonShowVisible){
+                    buttonShow.setVisibility(View.GONE);
+                    isButtonShowVisible = false;
+                }else {
+                    buttonShow.setVisibility(View.VISIBLE);
+                    isButtonShowVisible = true;
+                }
             }
         });
+        root.findViewById(R.id.aumento_vendas).setOnClickListener(clickListener);
+        root.findViewById(R.id.queda_vendas).setOnClickListener(clickListener);
+        root.findViewById(R.id.total_vendas).setOnClickListener(clickListener);
 
 
         return root;
