4ce6927469a61377dd84d6114cfd8d3af8460dc8
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-05-07 16:52:35 -0300

    Update - Filter by date
    Filter by date

--- app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeFragment.java
+++ app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeFragment.java
@@ -10,30 +10,16 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.google.android.material.snackbar.BaseTransientBottomBar;
-import com.google.android.material.snackbar.Snackbar;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.Random;
-
 import br.com.renandeldotti.inventoryapp.R;
-import br.com.renandeldotti.inventoryapp.database.QuantityAndPrice;
-import br.com.renandeldotti.inventoryapp.ui.sales.SalesViewModel;
 
 public class HomeFragment extends Fragment {
 
     private View tvRitmoVendas;
     private int isTvRitmoVendasVisible;
     private HomeViewModel homeViewModel;
-    private long totalSold = 0;
-    private TextView homeSales,weekSales,monthSales;
-    private String totalString = "";
+    private TextView todaySales,weekSales,monthSales;
 
     public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
@@ -42,30 +28,9 @@ public class HomeFragment extends Fragment {
         tvRitmoVendas = root.findViewById(R.id.home_rateSales_hidden);
         isTvRitmoVendasVisible = tvRitmoVendas.getVisibility();
 
-        homeSales = root.findViewById(R.id.home_todaySales_value);
+        todaySales = root.findViewById(R.id.home_todaySales_value);
         weekSales = root.findViewById(R.id.home_weekSales_value);
         monthSales = root.findViewById(R.id.home_monthSales_value);
-
-        try {
-            if (homeViewModel.getItemsSold(HomeViewModel.ONE_DAY) != null){
-                homeViewModel.getItemsSold(HomeViewModel.ONE_DAY).observe(getViewLifecycleOwner(), new Observer<List<QuantityAndPrice>>() {
-                    @Override
-                    public void onChanged(List<QuantityAndPrice> quantityAndPrices) {
-                        for (int i = 0; i < quantityAndPrices.size();i++){
-                            totalSold += quantityAndPrices.get(i).getQuantity_sold();
-                        }
-                        homeSales.setText(String.valueOf(totalSold));
-                        weekSales.setText(String.valueOf(totalSold));
-                        monthSales.setText(String.valueOf(totalSold));
-                    }
-                });
-            }
-        }catch (Exception e){
-            Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
-            if (getView()!=null)
-            Snackbar.make(getView(),"Error", BaseTransientBottomBar.LENGTH_SHORT).show();
-            Log.e(HomeFragment.class.getSimpleName(),"Error: " + e);
-        }
 
         root.findViewById(R.id.ritmo_vendas).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,8 +39,16 @@ public class HomeFragment extends Fragment {
                     tvRitmoVendas.setVisibility(View.GONE);
                     isTvRitmoVendasVisible = View.GONE;
                 }else {
+                    todaySales.setText(homeViewModel.getSales(HomeViewModel.ONE_DAY,getViewLifecycleOwner()));
+                    weekSales.setText(homeViewModel.getSales(HomeViewModel.ONE_WEEK,getViewLifecycleOwner()));
+                    monthSales.setText(homeViewModel.getSales(HomeViewModel.ONE_MONTH,getViewLifecycleOwner()));
+
                     tvRitmoVendas.setVisibility(View.VISIBLE);
                     isTvRitmoVendasVisible = View.VISIBLE;
+
+                    Log.e("Test :: Today sales: ", todaySales.getText().toString());
+                    Log.e("Test :: Week sales: ",weekSales.getText().toString());
+                    Log.e("Test :: Month sales: ",monthSales.getText().toString());
                 }
             }
         });
--- app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeViewModel.java
+++ app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeViewModel.java
@@ -1,10 +1,13 @@
 package br.com.renandeldotti.inventoryapp.ui.home;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 
 import java.util.List;
 
@@ -15,16 +18,42 @@ public class HomeViewModel extends AndroidViewModel {
 
     private SoldRepository soldRepository;
 
-    public static final long ONE_DAY = 86400000;
-    public static final long ONE_WEEK = 604800000;
-    public static final long ONE_MONTH = Long.parseLong("2628000000");
+    static final long ONE_DAY = 86400000;
+    static final long ONE_WEEK = 604800000;
+    static final long ONE_MONTH = Long.parseLong("2628000000");
+
+    private String quantityTotal = "";
 
     public HomeViewModel(@NonNull Application application) {
         super(application);
         soldRepository = new SoldRepository(application);
     }
 
-    public LiveData<List<QuantityAndPrice>> getItemsSold(long sinceDate) {
+    LiveData<List<QuantityAndPrice>> getItemsSold(long sinceDate) {
         return soldRepository.getSoldSince(sinceDate);
     }
+
+    String getSales(long since, LifecycleOwner owner){
+        try {
+            if (soldRepository.getSoldSince(since) != null){
+                soldRepository.getSoldSince(since).observe(owner, new Observer<List<QuantityAndPrice>>() {
+                    @Override
+                    public void onChanged(List<QuantityAndPrice> quantityAndPrices) {
+                        long longSolds = 0;
+                        for (int i =0; i< quantityAndPrices.size();i++){
+                            longSolds += quantityAndPrices.get(i).getQuantity_sold();
+                        }
+                        quantityTotal = String.valueOf(longSolds);
+                    }
+                });
+            }else {
+                quantityTotal = "0";
+            }
+        }catch (Exception e){
+            Log.e(HomeViewModel.class.getSimpleName(),"Error:\t"+e);
+            quantityTotal = "Error";
+        }
+        return quantityTotal;
+    }
+
 }
