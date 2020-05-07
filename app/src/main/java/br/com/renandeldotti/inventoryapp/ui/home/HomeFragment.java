2eed588d4923dcd785076028d90db63106740dd1
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-05-07 15:25:51 -0300

    Update

--- app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeFragment.java
+++ app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeFragment.java
@@ -9,18 +9,26 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
 
+import java.util.List;
 import java.util.Random;
 
 import br.com.renandeldotti.inventoryapp.R;
+import br.com.renandeldotti.inventoryapp.database.QuantityAndPrice;
+import br.com.renandeldotti.inventoryapp.ui.sales.SalesViewModel;
 
 public class HomeFragment extends Fragment {
 
     private View tvRitmoVendas;
     private int isTvRitmoVendasVisible;
+    private HomeViewModel homeViewModel;
+    private LiveData<List<QuantityAndPrice>> allSalesSince;
 
     public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        homeViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()).create(HomeViewModel.class);
 
         tvRitmoVendas = root.findViewById(R.id.home_rateSales_hidden);
         isTvRitmoVendasVisible = tvRitmoVendas.getVisibility();
@@ -31,7 +39,11 @@ public class HomeFragment extends Fragment {
 
         int i = new Random().nextInt(100);
 
-        homeSales.setText((i >= 60)?"5":"6");
+        allSalesSince = homeViewModel.getItemsSold(HomeViewModel.ONE_DAY);
+
+        List<QuantityAndPrice> solds = allSalesSince.getValue();
+
+        homeSales.setText("");
         weekSales.setText((i <= 40)?"50":"60");
         monthSales.setText((i > 40 && i < 60)?"500"+i:"600"+i);
 
--- app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeViewModel.java
+++ app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeViewModel.java
@@ -4,14 +4,27 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import br.com.renandeldotti.inventoryapp.database.QuantityAndPrice;
+import br.com.renandeldotti.inventoryapp.database.SoldRepository;
 
 public class HomeViewModel extends AndroidViewModel {
 
-    private long oneDayInMilliseconds = 86400000;
-    private long weekInMilliseconds = 604800000;
-    private long monthInMilliseconds = Long.parseLong("2628000000");
+    private SoldRepository soldRepository;
+
+    public static final long ONE_DAY = 86400000;
+    public static final long ONE_WEEK = 604800000;
+    public static final long ONE_MONTH = Long.parseLong("2628000000");
 
     public HomeViewModel(@NonNull Application application) {
         super(application);
+        soldRepository = new SoldRepository(application);
+    }
+
+    public LiveData<List<QuantityAndPrice>> getItemsSold(long sinceDate) {
+        return soldRepository.getSoldSince(sinceDate);
     }
 }
