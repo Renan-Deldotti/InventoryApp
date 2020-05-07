93d93d9610bb1b66ba12ae1ab01ccc27cc131c43
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-05-06 23:45:25 -0300

    Update

--- app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeFragment.java
+++ app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+
+import java.util.Random;
 
 import br.com.renandeldotti.inventoryapp.R;
 
@@ -19,8 +22,18 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        tvRitmoVendas = root.findViewById(R.id.home_buttonShow);
+        tvRitmoVendas = root.findViewById(R.id.home_rateSales_hidden);
         isTvRitmoVendasVisible = tvRitmoVendas.getVisibility();
+
+        TextView homeSales = root.findViewById(R.id.home_todaySales_value);
+        TextView weekSales = root.findViewById(R.id.home_weekSales_value);
+        TextView monthSales = root.findViewById(R.id.home_monthSales_value);
+
+        int i = new Random().nextInt(100);
+
+        homeSales.setText((i >= 60)?"5":"6");
+        weekSales.setText((i <= 40)?"50":"60");
+        monthSales.setText((i > 40 && i < 60)?"500"+i:"600"+i);
 
         root.findViewById(R.id.ritmo_vendas).setOnClickListener(new View.OnClickListener() {
             @Override
