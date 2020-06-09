2c56b5df1e9f24231879c1fda53d6684fa1a13a5
Renan Deldotti <35459237+Renan-Deldotti@users.noreply.github.com>
2020-06-08 23:48:41 -0300

    Update HomeFragment.java
    Updated to viewBinding

--- app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeFragment.java
+++ app/src/main/java/br/com/renandeldotti/inventoryapp/ui/home/HomeFragment.java
@@ -28,10 +28,10 @@ public class HomeFragment extends Fragment {
     private FragmentHomeBinding homeBinding;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        homeBinding = FragmentHomeBinding.inflate(inflater,container,false);
+        homeBinding = FragmentHomeBinding.inflate(inflater, container, false);
         final View root = homeBinding.getRoot();
-        if(getActivity() != null)
-        homeViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()).create(HomeViewModel.class);
+        if (getActivity() != null)
+            homeViewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(getActivity().getApplication()).create(HomeViewModel.class);
 
         homeBinding.ritmoVendas.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,28 +82,24 @@ public class HomeFragment extends Fragment {
         });
 
 
-        root.findViewById(R.id.aumento_vendas).setOnClickListener(new View.OnClickListener() {
+        homeBinding.aumentoVendas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final View parentView = root.findViewById(R.id.home_salesRise_hidden);
+                final View parentView = homeBinding.homeSalesRiseHidden;
 
                 if (homeViewModel.getProductsSorted() != null) {
                     homeViewModel.getProductsSorted().observe(getViewLifecycleOwner(), new Observer<List<Products>>() {
                         @Override
                         public void onChanged(List<Products> products) {
-                            TextView salesRiseFirstName = root.findViewById(R.id.home_salesRise_first_name);
-                            TextView salesRiseFirstValue = root.findViewById(R.id.home_salesRise_first_value);
-                            TextView salesRiseSecondName = root.findViewById(R.id.home_salesRise_second_name);
-                            TextView salesRiseSecondValue = root.findViewById(R.id.home_salesRise_second_value);
                             if (parentView.getVisibility() == View.GONE || parentView.getVisibility() == View.INVISIBLE) {
                                 if (products.size() == 1) {
-                                    salesRiseFirstName.setText(products.get(0).getProduct_name());
-                                    salesRiseFirstValue.setText(String.valueOf(products.get(0).getQuantity_sold()));
+                                    homeBinding.homeSalesRiseFirstName.setText(products.get(0).getProduct_name());
+                                    homeBinding.homeSalesRiseFirstValue.setText(String.valueOf(products.get(0).getQuantity_sold()));
                                 } else if (products.size() > 1) {
-                                    salesRiseFirstName.setText(products.get(0).getProduct_name());
-                                    salesRiseFirstValue.setText(String.valueOf(products.get(0).getQuantity_sold()));
-                                    salesRiseSecondName.setText(products.get(1).getProduct_name());
-                                    salesRiseSecondValue.setText(String.valueOf(products.get(1).getQuantity_sold()));
+                                    homeBinding.homeSalesRiseFirstName.setText(products.get(0).getProduct_name());
+                                    homeBinding.homeSalesRiseFirstValue.setText(String.valueOf(products.get(0).getQuantity_sold()));
+                                    homeBinding.homeSalesRiseSecondName.setText(products.get(1).getProduct_name());
+                                    homeBinding.homeSalesRiseSecondValue.setText(String.valueOf(products.get(1).getQuantity_sold()));
                                 }
                                 parentView.setVisibility(View.VISIBLE);
                             } else {
@@ -116,28 +112,24 @@ public class HomeFragment extends Fragment {
         });
 
 
-        root.findViewById(R.id.queda_vendas).setOnClickListener(new View.OnClickListener() {
+        homeBinding.quedaVendas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final View parentView = root.findViewById(R.id.home_salesDrop_hidden);
+                final View parentView = homeBinding.homeSalesDropHidden;
                 if (homeViewModel.getProductsSorted() != null) {
                     homeViewModel.getProductsSorted().observe(getViewLifecycleOwner(), new Observer<List<Products>>() {
                         @Override
                         public void onChanged(List<Products> products) {
                             int arrSize = products.size();
-                            TextView salesDropFirstName = root.findViewById(R.id.home_salesDrop_first_name);
-                            TextView salesDropFirstValue = root.findViewById(R.id.home_salesDrop_first_value);
-                            TextView salesDropSecondName = root.findViewById(R.id.home_salesDrop_second_name);
-                            TextView salesDropSecondValue = root.findViewById(R.id.home_salesDrop_second_value);
                             if (parentView.getVisibility() == View.GONE) {
                                 if (arrSize > 1) {
-                                    salesDropFirstName.setText(products.get(arrSize - 1).getProduct_name());
-                                    salesDropFirstValue.setText(String.valueOf(products.get(arrSize - 1).getQuantity_sold()));
-                                    salesDropSecondName.setText(products.get(arrSize - 2).getProduct_name());
-                                    salesDropSecondValue.setText(String.valueOf(products.get(arrSize - 2).getQuantity_sold()));
+                                    homeBinding.homeSalesDropFirstName.setText(products.get(arrSize - 1).getProduct_name());
+                                    homeBinding.homeSalesDropFirstValue.setText(String.valueOf(products.get(arrSize - 1).getQuantity_sold()));
+                                    homeBinding.homeSalesDropSecondName.setText(products.get(arrSize - 2).getProduct_name());
+                                    homeBinding.homeSalesDropSecondValue.setText(String.valueOf(products.get(arrSize - 2).getQuantity_sold()));
                                 } else if (arrSize == 1) {
-                                    salesDropFirstName.setText(products.get(arrSize - 1).getProduct_name());
-                                    salesDropFirstValue.setText(String.valueOf(products.get(arrSize - 1).getQuantity_sold()));
+                                    homeBinding.homeSalesDropFirstName.setText(products.get(arrSize - 1).getProduct_name());
+                                    homeBinding.homeSalesDropFirstValue.setText(String.valueOf(products.get(arrSize - 1).getQuantity_sold()));
                                 }
                                 parentView.setVisibility(View.VISIBLE);
                             } else {
@@ -150,74 +142,68 @@ public class HomeFragment extends Fragment {
         });
 
 
-        root.findViewById(R.id.total_vendas).setOnClickListener(new View.OnClickListener() {
+        homeBinding.totalVendas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                View parentView = root.findViewById(R.id.home_profit_hidden);
-                if(parentView.getVisibility() == View.GONE || parentView.getVisibility() == View.INVISIBLE){
+                View parentView = homeBinding.homeProfitHidden;
+                if (parentView.getVisibility() == View.GONE || parentView.getVisibility() == View.INVISIBLE) {
                     boolean hasProfit = false;
 
-                    if (homeViewModel.getSalesSince(HomeViewModel.ONE_DAY) != null){
+                    if (homeViewModel.getSalesSince(HomeViewModel.ONE_DAY) != null) {
                         homeViewModel.getSalesSince(HomeViewModel.ONE_DAY).observe(getViewLifecycleOwner(), new Observer<List<QuantityAndPrice>>() {
                             @Override
                             public void onChanged(List<QuantityAndPrice> quantityAndPrices) {
-                                TextView dailyProfit = root.findViewById(R.id.daily_profit);
                                 float totalProfit = 0f;
-                                for (QuantityAndPrice quantityAndPrice : quantityAndPrices){
+                                for (QuantityAndPrice quantityAndPrice : quantityAndPrices) {
                                     totalProfit += (quantityAndPrice.getQuantity_sold() * quantityAndPrice.getSold_price());
                                 }
-                                String formattedValue = Currency.getInstance(Locale.getDefault()).getSymbol() + String.format(Locale.getDefault(),"%.2f",totalProfit);
-                                dailyProfit.setText(formattedValue);
+                                String formattedValue = Currency.getInstance(Locale.getDefault()).getSymbol() + String.format(Locale.getDefault(), "%.2f", totalProfit);
+                                homeBinding.dailyProfit.setText(formattedValue);
                             }
                         });
                         hasProfit = true;
-                    }else{
-                        View linearLayoutDaily = root.findViewById(R.id.linearLayout_daily_profit);
-                        linearLayoutDaily.setVisibility(View.GONE);
+                    } else {
+                        homeBinding.linearLayoutDailyProfit.setVisibility(View.GONE);
                     }
 
-                    if (homeViewModel.getSalesSince(HomeViewModel.ONE_WEEK) != null){
+                    if (homeViewModel.getSalesSince(HomeViewModel.ONE_WEEK) != null) {
                         homeViewModel.getSalesSince(HomeViewModel.ONE_WEEK).observe(getViewLifecycleOwner(), new Observer<List<QuantityAndPrice>>() {
                             @Override
                             public void onChanged(List<QuantityAndPrice> quantityAndPrices) {
-                                TextView weeklyProfit = root.findViewById(R.id.weekly_profit);
                                 float totalProfit = 0f;
-                                for (QuantityAndPrice quantityAndPrice : quantityAndPrices){
+                                for (QuantityAndPrice quantityAndPrice : quantityAndPrices) {
                                     totalProfit += (quantityAndPrice.getQuantity_sold() * quantityAndPrice.getSold_price());
                                 }
-                                String formattedValue = Currency.getInstance(Locale.getDefault()).getSymbol() + String.format(Locale.getDefault(),"%.2f",totalProfit);
-                                weeklyProfit.setText(formattedValue);
+                                String formattedValue = Currency.getInstance(Locale.getDefault()).getSymbol() + String.format(Locale.getDefault(), "%.2f", totalProfit);
+                                homeBinding.weeklyProfit.setText(formattedValue);
                             }
                         });
                         hasProfit = true;
-                    }else {
-                        View linearLayoutWeekly = root.findViewById(R.id.linearLayout_weekly_profit);
-                        linearLayoutWeekly.setVisibility(View.GONE);
+                    } else {
+                        homeBinding.linearLayoutWeeklyProfit.setVisibility(View.GONE);
                     }
 
-                    if (homeViewModel.getSalesSince(HomeViewModel.ONE_MONTH) != null){
+                    if (homeViewModel.getSalesSince(HomeViewModel.ONE_MONTH) != null) {
                         homeViewModel.getSalesSince(HomeViewModel.ONE_MONTH).observe(getViewLifecycleOwner(), new Observer<List<QuantityAndPrice>>() {
                             @Override
                             public void onChanged(List<QuantityAndPrice> quantityAndPrices) {
-                                TextView monthlyProfit = root.findViewById(R.id.monthly_profit);
                                 float totalProfit = 0f;
-                                for (QuantityAndPrice quantityAndPrice : quantityAndPrices){
+                                for (QuantityAndPrice quantityAndPrice : quantityAndPrices) {
                                     totalProfit += (quantityAndPrice.getQuantity_sold() * quantityAndPrice.getSold_price());
                                 }
-                                String formattedValue = Currency.getInstance(Locale.getDefault()).getSymbol() + String.format(Locale.getDefault(),"%.2f",totalProfit);
-                                monthlyProfit.setText(formattedValue);
+                                String formattedValue = Currency.getInstance(Locale.getDefault()).getSymbol() + String.format(Locale.getDefault(), "%.2f", totalProfit);
+                                homeBinding.monthlyProfit.setText(formattedValue);
                             }
                         });
                         hasProfit = true;
-                    }else {
-                        View linearLayoutMonthly = root.findViewById(R.id.linearLayout_monthly_profit);
-                        linearLayoutMonthly.setVisibility(View.GONE);
+                    } else {
+                        homeBinding.linearLayoutMonthlyProfit.setVisibility(View.GONE);
                     }
 
-                    if (hasProfit){
+                    if (hasProfit) {
                         parentView.setVisibility(View.VISIBLE);
                     }
-                }else{
+                } else {
                     parentView.setVisibility(View.GONE);
                 }
             }
